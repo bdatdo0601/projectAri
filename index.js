016295053491d677243d36dcddbfff846fac7294
bdatdo0601 <dod2@wit.edu>
2016-12-23 01:03:42 -0500

    Voice Recognition is now working!
    This is contructed using google cloud speech API. the process right now is the Aira will hear a keyword (whether Alexa or Hey Justin) then it will trigger writing to a file.
    the recording will stop either when there is silence or it reach 5 seconds limit. since Snowboy can do multiple keyword. Maybe should integrate Alexa and in the future Google Assistant?
    
    TODO: Integrate API.AI tomorrow.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const record = require('node-record-lpcm16');
 const snowboy = require('snowboy');
 const player = require('play-sound')(opts = {});
+const fs = require('fs');
+const google_speech = require('google-speech'); //this is use for text to speech
+const speech = require('@google-cloud/speech')({
+  projectID: 'projectari-153322',
+  keyFilename: 'resources/projectAri-d1fc30912718.json',
+});
 
 const models = new snowboy.Models();
+var isDetected = false;
+var file;
+var requestPath;
 
 models.add({
   file: 'resources/alexa.umdl',
-  sensitivity: '0.5',
+  sensitivity: '.1',
   hotwords : 'alexa'
 });
+
+models.add({
+  file: 'resources/Hey_Justin.pmdl',
+  hotwords: 'justin'
+})
 
 const detector = new snowboy.Detector({
   resource: "resources/common.res",
@@ -16,26 +30,38 @@ const detector = new snowboy.Detector({
   audioGain: 2.0
 });
 
-detector.on('silence', function () {
+detector.on('silence', function() {
+  if(isDetected){
+    file.end();
+    playSoundFile('resources/ding.wav');
+    isDetected = false;
+    processRequest(requestPath);
+  }
 });
 
 detector.on('sound', function () {
-  console.log('sound');
 });
 
-detector.on('error', function () {
+detector.on('error', function (err) {
   console.log('error');
+  console.log(err);
 });
 
 detector.on('hotword', function (index, hotword) {
-  console.log('hotword', index, hotword);
-  playSoundFile('resources/dong.wav');
+  console.log(isDetected);
+  if (!isDetected){
+    console.log('hotword', index, hotword);
+    playSoundFile('resources/dong.wav');
+    makeRequest('resources/request.wav');
+  }
 });
 
-const mic = record.start({
+var mic = record.start({
   threshold: 0,
   verbose: false
 });
+
+mic.pipe(detector);
 
 const playSoundFile = (filePath) => {
   console.log(`${filePath} is played`);
@@ -44,4 +70,31 @@ const playSoundFile = (filePath) => {
   })
 }
 
-mic.pipe(detector);
+const makeRequest = (filePath) => {
+  requestPath = filePath;
+  file = fs.createWriteStream(filePath);
+  record.start().pipe(file)
+  setTimeout(() => {isDetected = true;}, 1000);
+  setTimeout(()=>{
+    if (isDetected){
+      file.end();
+      playSoundFile('resources/ding.wav');
+      mic.pipe(detector);
+      isDetected = false;
+      processRequest(requestPath);
+    }
+  }, 5000);
+}
+
+const processRequest = (filePath) => {
+  speech.recognize(filePath, {
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+  },function(err, transcript){
+    if (err){
+      console.log(err);
+    } else {
+      console.log(transcript);
+    }
+  });
+};
